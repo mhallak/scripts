c6f8660b6cb7b6ff5be18a5fa310ef6dddafee27
Michele Hallak <mhallak@xicore.net>
2016-01-18 16:18:16 +0200

    Kill OCU and don't kill chrome in script killrobilros.sh

--- killrobilros.sh
+++ killrobilros.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ "$(ps -ef | grep -i ros | grep -v grep | grep -v killrobilros.sh | awk '{print $2}')" ]; then # or $(rosnode list)
-    for i in $(ps -ef | grep -i ros | grep -v grep | grep -v killrobilros.sh | awk '{print $2}')
+if [ "$(ps -ef | grep -i ros | grep -v grep | grep -v killrobilros.sh | grep -v chrome | awk '{print $2}')" ]; then # or $(rosnode list)
+    for i in $(ps -ef | grep -i ros | grep -v grep | grep -v killrobilros.sh | grep -v chrome | awk '{print $2}')
     do
         kill -9 $i;
         echo "Killing " $i "...killed";
+    done
+fi
+if [ "$(ps -ef | grep ocu | grep -v grep | awk '{print $2}')" ]; then # or $(rosnode list)
+    for i in $(ps -ef | grep ocu | grep -v grep | awk '{print $2}')
+    do
+        kill -9 $i;
+        echo "Killing OCU " $i "...killed";
     done
 fi
