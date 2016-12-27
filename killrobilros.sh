645a0b256df6c3fb717290178063be8a734db501
Michele Hallak <mhallak@xicore.net>
2016-12-27 13:18:45 +0200

    Correct killrobil

--- killrobilros.sh
+++ killrobilros.sh
@@ -7,10 +7,11 @@ if [ "$(ps -ef | grep -i ros | grep -v grep | grep -v killrobilros.sh | grep -v 
         echo "Killing " $i "...killed";
     done
 fi
-if [ "$(ps -ef | grep ocu | grep -v grep | awk '{print $2}')" ]; then # or $(rosnode list)
-    for i in $(ps -ef | grep ocu | grep -v grep | awk '{print $2}')
+#if [ "$(ps -ef | grep ocu | grep -v grep | grep -v chrome | grep -v ocum | awk '{print $2}')" ]; then # or $(rosnode list)
+if [ "$(ps -ef | grep ocu.py | grep -v grep | awk '{print $2}')" ]; then # or $(rosnode list)
+    for j in $(ps -ef | grep ocu.py | grep -v grep | awk '{print $2}')
     do
-        kill -9 $i;
-        echo "Killing OCU " $i "...killed";
+        kill -9 $j;
+        echo "Killing OCU " $j "...killed";
     done
 fi
