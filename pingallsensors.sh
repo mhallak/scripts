068c38ee7c93d9fb10e81c78e2820c85fdfc907f
Daniel Meltz <daniel.meltz@gmail.com>
2018-12-24 15:31:46 +0200

    grrr

--- pingallsensors.sh
+++ pingallsensors.sh
@@ -1,10 +1,10 @@
 #
 
 echo "pinging shiffon"
-ping 132.4.6.60
+ping  -c 5 132.4.6.60
 #sleep 5
 echo "pinging IBEO"
-ping 192.168.0.6
+ping -c 5 192.168.0.6
 #sleep 5
 echo "pinging kinetic"
-ping 192.168.101.3
+ping -c 5  192.168.101.3
